8a3abbba13ec8e4574a7f64c2aa75727631ed476
Mike Bell <m.bell@techsmith.com>
2020-12-01 00:19:17 -0500

    Day 1

--- 01/solution.js
+++ 01/solution.js
@@ -1,7 +1,32 @@
-const parseInput = input => input;
+const parseInput = input => input.split('\n').map(n => +n);
 
-const runPart1 = input => input;
+const runPart1 = input => {
+   for (let i = 0; i < input.length; i++) {
+      for (let j = i + 1; j < input.length; j++) {
+         const a = input[i];
+         const b = input[j];
 
-const runPart2 = input => input;
+         if (a + b === 2020) {
+            return a * b;
+         }
+      }
+   }
+};
+
+const runPart2 = input => {
+   for (let i = 0; i < input.length; i++) {
+      for (let j = i + 1; j < input.length; j++) {
+         for (let k = j + 1; k < input.length; k++) {
+            const a = input[i];
+            const b = input[j];
+            const c = input[k];
+
+            if (a + b + c === 2020) {
+               return a * b * c;
+            }
+         }
+      }
+   }
+};
 
 module.exports = {parseInput, runPart1, runPart2};
