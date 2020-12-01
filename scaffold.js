0f93de9c19ada18e1d4b1590a078117932106739
Mike Bell <m.bell@techsmith.com>
2020-11-30 22:05:26 -0500

    Scaffold all 31 days and add scaffold script

---
+++ scaffold.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const range = require('./utils/range');
+
+range(1, 32).forEach(i => {
+   const paddedDay = i < 10 ? `0${i}` : `${i}`;
+   const folderPath = `./${paddedDay}`;
+   if (!fs.existsSync(folderPath)){
+      fs.mkdirSync(folderPath);
+  }
+
+  if (!fs.existsSync('./inputs')){
+   fs.mkdirSync('./inputs');
+}
+
+  fs.copyFileSync('./solution-template.js', `${folderPath}/solution.js`);
+  fs.copyFileSync('./input-template.txt', `./inputs/input${paddedDay}.txt`);
+});
