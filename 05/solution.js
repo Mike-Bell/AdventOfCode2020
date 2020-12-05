b64ddd778ba7f0ac599b98b9b82fdb092f7bc5f2
Mike Bell <m.bell@techsmith.com>
2020-12-05 09:13:53 -0500

    Day 5

--- 05/solution.js
+++ 05/solution.js
@@ -1,7 +1,28 @@
-const parseInput = input => input;
+const parseInput = input => input.split('\r\n').map(line => line.split('').map(letter => letter === 'F' || letter === 'L'));
 
-const runPart1 = input => input;
+const getIndexFromPartitions = partitions => {
+   let [min, max] = [0, 2 ** partitions.length - 1];
+   partitions.forEach(p => {
+      if (p) {
+         max = (max - min + 1) / 2 + min - 1;
+      } else {
+         min = (max - min + 1) / 2 + min;
+      }
+   });
+   return min;
+};
 
-const runPart2 = input => input;
+const getIdFromPartitions = partitions => getIndexFromPartitions(partitions.slice(0, 7)) * 8 + getIndexFromPartitions(partitions.slice(7, 10));
+
+const runPart1 = input => Math.max(...input.map(getIdFromPartitions));
+
+const runPart2 = input => {
+   const ids = input.map(getIdFromPartitions).sort();
+   for (let i = 0; i < ids.length - 1; i++) {
+      if (ids[i + 1] - ids[i] === 2) {
+         return ids[i + 1] - 1;
+      }
+   }
+};
 
 module.exports = {parseInput, runPart1, runPart2};
