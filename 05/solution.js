eef91601ca9a9e0127dd047655dbc4f3c94d79ab
Mike Bell <m.bell@techsmith.com>
2020-12-05 09:29:13 -0500

    Simplify a bit

--- 05/solution.js
+++ 05/solution.js
@@ -1,16 +1,9 @@
 const parseInput = input => input.split('\r\n').map(line => line.split('').map(letter => letter === 'F' || letter === 'L'));
 
-const getIndexFromPartitions = partitions => {
-   let [min, max] = [0, 2 ** partitions.length - 1];
-   partitions.forEach(p => {
-      if (p) {
-         max = (max - min + 1) / 2 + min - 1;
-      } else {
-         min = (max - min + 1) / 2 + min;
-      }
-   });
-   return min;
-};
+const getIndexFromPartitions = partitions => partitions.reduce(([min, max], p) => p
+   ? [min, (max - min + 1) / 2 + min - 1]
+   : [(max - min + 1) / 2 + min, max]
+, [0, 2 ** partitions.length - 1])[0];
 
 const getIdFromPartitions = partitions => getIndexFromPartitions(partitions.slice(0, 7)) * 8 + getIndexFromPartitions(partitions.slice(7, 10));
 
