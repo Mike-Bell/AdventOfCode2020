9c1dd2ced50f9b7d85c25cdeb9fc6747a2b2ebf9
Mike Bell <mikel3377@gmail.com>
2020-12-09 00:20:13 -0500

    Day 9

--- 09/solution.js
+++ 09/solution.js
@@ -1,7 +1,47 @@
-const parseInput = input => input;
+const parseInput = input => input.split('\r\n').map(n => +n);
 
-const runPart1 = input => input;
+const getInvalidNum = nums => {
+    const candidates = nums.slice(25);
+    for (let i = 0; i < candidates.length; i++) {
+        const adders = nums.slice(i, i + 25);
+        let isValid = false;
+        
+        isValid:
+        for (let j = 0; j < adders.length; j++) {
+            for (let k = 0; k < adders.length; k++) {
+                if (j === k) {
+                    continue;
+                }
 
-const runPart2 = input => input;
+                if (adders[j] + adders[k] === candidates[i]) {
+                    isValid = true;
+                    break isValid;
+                }
+            }
+        }
+        if (!isValid) {
+            return candidates[i];
+        }
+    }
+};
+
+const runPart1 = nums => getInvalidNum(nums);
+
+const runPart2 = nums => {
+    const target = getInvalidNum(nums);
+    for (let i = 0; i < nums.length; i++) {
+        let sum = 0;
+        let j = 0;
+        while (sum < target) {
+            sum += nums[i + j];
+            j++;
+
+            if (sum === target) {
+                const hits = nums.slice(i, i + j + 1);
+                return Math.max(...hits) + Math.min(...hits);
+            }
+        }
+    }
+};
 
 module.exports = {parseInput, runPart1, runPart2};
